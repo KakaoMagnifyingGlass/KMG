d9f3528164b8a303aed9e9a588f5d1a4ab1b78f3
juhee067 <66045537+juhee067@users.noreply.github.com>
2023-04-25 01:13:37 +0900

    feat:date기능 redux 저장

--- src/store/configStore.tsx
+++ src/store/configStore.tsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { limitTimeSlice } from "./reducer/limitTimeSlice";
 import { analyzedMessagesSlice } from "./reducer/messageSlice";
 
 export default configureStore({
   reducer: {
     analyzedMessagesSlice: analyzedMessagesSlice.reducer,
+    limitTimeSlice: limitTimeSlice.reducer,
   },
 });
