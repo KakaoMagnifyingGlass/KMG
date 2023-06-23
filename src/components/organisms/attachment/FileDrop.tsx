leesj-dev/kakaotalk-stats

refactor: 파일 폴더로 세분화

src/components/organisms/attachment/FileDrop.tsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import AttachmentButton from "../atoms/AttachmentButton";
-import Paragraph from "../atoms/Paragraph";
-import Span from "../atoms/Span";
+import AttachmentButton from "../../atoms/AttachmentButton";
+import Paragraph from "../../atoms/Paragraph";
+import Span from "../../atoms/Span";
 import OsList from "./OsList";
 import { useDispatch } from "react-redux";
-import { pushNewlyAttachedFiles } from "../../store/reducer/attachedFileListSlice";
-import { FlexColumnCenterDiv } from "../styleComponents/FlexDiv";
+import { pushNewlyAttachedFiles } from "../../../store/reducer/attachment/attachedFileListSlice";
+import { FlexColumnCenterDiv } from "../../styleComponents/FlexDiv";
 
 const DropBox = styled(FlexColumnCenterDiv)`
   position: relative;

src/components/organisms/dashboard/GraphDisplay.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
-import Span from "../atoms/Span";
-import Icon from "../atoms/Icon";
-import ChatRatioWithArrowGraph from "../molecules/graphs/ChatRatioWithArrowGraph";
-import ChatRoomCompareGraph from "../molecules/graphs/ChatRoomCompareGraph";
-import ChatVolumeByPeriodGraph from "../molecules/graphs/ChatVolumeByPeriodGraph";
-import ChatRateGraph from "../molecules/graphs/ChatRateGraph";
-import ReplyCountByHourlyGraph from "../molecules/graphs/ReplyCountByHourlyGraph";
-import KeywordChartGraph from "../molecules/graphs/KeywordChartGraph";
-import ReplySpeedGraph from "../molecules/graphs/ReplySpeedGraph";
-import ChatVolumeByHourlyGraph from "../molecules/graphs/ChatVolumeByHourlyGraph";
+import Span from "../../atoms/Span";
+import Icon from "../../atoms/Icon";
+import ChatRatioWithArrowGraph from "../../molecules/graphs/ChatRatioWithArrowGraph";
+import ChatRoomCompareGraph from "../../molecules/graphs/ChatRoomCompareGraph";
+import ChatVolumeByPeriodGraph from "../../molecules/graphs/ChatVolumeByPeriodGraph";
+import ChatRateGraph from "../../molecules/graphs/ChatRateGraph";
+import ReplyCountByHourlyGraph from "../../molecules/graphs/ReplyCountByHourlyGraph";
+import KeywordChartGraph from "../../molecules/graphs/KeywordChartGraph";
+import ReplySpeedGraph from "../../molecules/graphs/ReplySpeedGraph";
+import ChatVolumeByHourlyGraph from "../../molecules/graphs/ChatVolumeByHourlyGraph";
 import { CgMaximize } from "react-icons/cg";
-import { setIsModalVisible } from "../../store/reducer/isModalVisibleSlice";
-import { FlexColumnCenterDiv } from "../styleComponents/FlexDiv";
+import { setIsModalVisible } from "../../../store/reducer/isModalVisibleSlice";
+import { FlexColumnCenterDiv } from "../../styleComponents/FlexDiv";
 
 const graphContentData = [
   {

src/components/organisms/dashboard/ModalGraph.tsx
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import Span from "../atoms/Span";
-import Icon from "../atoms/Icon";
-import ChatRatioWithArrowGraph from "../molecules/graphs/ChatRatioWithArrowGraph";
-import SpeakerSelect from "../molecules/SpeakerSelect";
+import Span from "../../atoms/Span";
+import Icon from "../../atoms/Icon";
+import ChatRatioWithArrowGraph from "../../molecules/graphs/ChatRatioWithArrowGraph";
+import SpeakerSelect from "../../molecules/dashboard/SpeakerSelect";
 import { useLocation } from "react-router";
 import { MdClose } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
-import { AnalyzedMessage } from "../../@types/index.d";
-import { getDates } from "../../module/common/getProperties";
-import { setIsModalVisible } from "../../store/reducer/isModalVisibleSlice";
-import { graphContentData } from "../pages/DetailPage";
+import { AnalyzedMessage } from "../../../@types/index.d";
+import { getDates } from "../../../module/common/getProperties";
+import { setIsModalVisible } from "../../../store/reducer/isModalVisibleSlice";
+import { graphContentData } from "../../pages/DetailPage";
 import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
-import { FlexCenterDiv } from "../styleComponents/FlexDiv";
-import Paragraph from "../atoms/Paragraph";
+import { FlexCenterDiv } from "../../styleComponents/FlexDiv";
+import Paragraph from "../../atoms/Paragraph";
 
 const ModalGraphBox = styled.div`
   padding: 20px 20px 30px 20px;

src/components/organisms/main/MainVisual.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import Paragraph from "../atoms/Paragraph";
-import Img from "../atoms/Img";
+import Paragraph from "../../atoms/Paragraph";
+import Img from "../../atoms/Img";
 import { useNavigate } from "react-router-dom";
-import ScrollIndicator from "../molecules/ScrollIndicator";
-import BlueButton from "../atoms/BlueButton";
-import { FlexCenterDiv } from "../styleComponents/FlexDiv";
+import ScrollIndicator from "../../molecules/ScrollIndicator";
+import BlueButton from "../../atoms/BlueButton";
+import { FlexCenterDiv } from "../../styleComponents/FlexDiv";
 import { useSelector } from "react-redux";
 
 const MainVisualContainer = styled(FlexCenterDiv)`
@@ -51,7 +51,9 @@ const AsideBox = styled.div`
     position: absolute;
     left: 50%;
     transform: translateX(-50%);
-    bottom: -50px;
+    @media (max-width: 768px) {
+      bottom: 0px;
+    }
   }
 `;
 

src/components/sections/attachment/AttachDescriptionSection.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
-import BlueButton from "../atoms/BlueButton";
-import Paragraph from "../atoms/Paragraph";
-import ThreeImages, { CardData } from "../organisms/ThreeImages";
-import scrollToEvent from "../../module/common/scrollToEvent";
-import { FlexCenterDiv, FlexColumnCenterDiv } from "../styleComponents/FlexDiv";
+import BlueButton from "../../atoms/BlueButton";
+import Paragraph from "../../atoms/Paragraph";
+import ThreeImages, { CardData } from "../../organisms/attachment/ThreeImages";
+import scrollToEvent from "../../../module/common/scrollToEvent";
+import { FlexCenterDiv, FlexColumnCenterDiv } from "../../styleComponents/FlexDiv";
 import { useLocation } from "react-router";
 
 const AttachDescriptionBox = styled(FlexColumnCenterDiv)`

src/components/sections/attachment/AttachmentSection.tsx
@@ -1,16 +1,16 @@
 import React, { useRef } from "react";
 import styled from "styled-components";
-import AttachedFileList from "../molecules/AttachedFileList";
-import BlueButton from "../atoms/BlueButton";
-import ScrollIndicator from "../molecules/ScrollIndicator";
-import FileDrop from "../organisms/FileDrop";
+import AttachedFileList from "../../molecules/attachment/AttachedFileList";
+import BlueButton from "../../atoms/BlueButton";
+import ScrollIndicator from "../../molecules/ScrollIndicator";
+import FileDrop from "../../organisms/attachment/FileDrop";
 import {
   AnalyzedMessage,
   Chatroom,
   FileObject,
   MessageInfo,
   OriginMessageData,
-} from "../../@types/index.d";
+} from "../../../@types/index.d";
 
 import {
   breakdownTxtFileAndroid,
@@ -18,18 +18,18 @@ import {
   breakdownTxtFileMacOS,
   breakdownTxtFileWindow,
   readAsDataURL,
-} from "../../module/core/breakdownTxtFile";
-import { getMessageData } from "../../module/core/getMessageData";
+} from "../../../module/core/breakdownTxtFile";
+import { getMessageData } from "../../../module/core/getMessageData";
 import { useDispatch, useSelector } from "react-redux";
-import { setAnalyzedMessages } from "../../store/reducer/analyzedMessagesSlice";
-import Span from "../atoms/Span";
+import { setAnalyzedMessages } from "../../../store/reducer/analyzedMessagesSlice";
+import Span from "../../atoms/Span";
 import { useNavigate } from "react-router";
-import scrollToEvent from "../../module/common/scrollToEvent";
-import OsList from "../organisms/OsList";
-import { pushNewlyAttachedFiles } from "../../store/reducer/attachedFileListSlice";
-import { setIsAnalyzedMessagesExist } from "../../store/reducer/isAnalyzedMessagesExistSlice";
-import Paragraph from "../atoms/Paragraph";
-import { FlexColumnCenterDiv } from "../styleComponents/FlexDiv";
+import scrollToEvent from "../../../module/common/scrollToEvent";
+import { pushNewlyAttachedFiles } from "../../../store/reducer/attachment/attachedFileListSlice";
+import { setIsAnalyzedMessagesExist } from "../../../store/reducer/isAnalyzedMessagesExistSlice";
+import Paragraph from "../../atoms/Paragraph";
+import { FlexColumnCenterDiv } from "../../styleComponents/FlexDiv";
+import OsList from "../../organisms/attachment/OsList";
 
 const AttachmentSectionBox = styled(FlexColumnCenterDiv)`
   margin: 80px auto 0 auto;

KakaoMagnifyingGlass/KMG

src/components/organisms/attachment/FileDrop.tsx
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import AttachmentButton from "../atoms/AttachmentButton";
-import Paragraph from "../atoms/Paragraph";
-import Span from "../atoms/Span";
+import AttachmentButton from "../../atoms/AttachmentButton";
+import Paragraph from "../../atoms/Paragraph";
+import Span from "../../atoms/Span";
 import OsList from "./OsList";
-import { useDispatch } from "react-redux";
-import { pushNewlyAttachedFiles } from "../../store/reducer/attachedFileListSlice";
-import { FlexColumnCenterDiv } from "../atoms/FlexDiv";
+import { FlexColumnCenterDiv } from "../../atoms/FlexDiv";
 
 const DropBox = styled(FlexColumnCenterDiv)`
   position: relative;

src/components/organisms/dashboard/GraphDisplay.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
-import Span from "../atoms/Span";
-import Icon from "../atoms/Icon";
-import ChatRatioWithArrowGraph from "../molecules/graphs/ChatRatioWithArrowGraph";
-import ChatRoomCompareGraph from "../molecules/graphs/ChatRoomCompareGraph";
-import ChatVolumeByPeriodGraph from "../molecules/graphs/ChatVolumeByPeriodGraph";
-import ChatRateGraph from "../molecules/graphs/ChatRateGraph";
-import ReplyCountByHourlyGraph from "../molecules/graphs/ReplyCountByHourlyGraph";
-import KeywordChartGraph from "../molecules/graphs/KeywordChartGraph";
-import ReplySpeedGraph from "../molecules/graphs/ReplySpeedGraph";
-import ChatVolumeByHourlyGraph from "../molecules/graphs/ChatVolumeByHourlyGraph";
+import Span from "../../atoms/Span";
+import Icon from "../../atoms/Icon";
+import ChatRatioWithArrowGraph from "../../molecules/graphs/ChatRatioWithArrowGraph";
+import ChatRoomCompareGraph from "../../molecules/graphs/ChatRoomCompareGraph";
+import ChatVolumeByPeriodGraph from "../../molecules/graphs/ChatVolumeByPeriodGraph";
+import ChatRateGraph from "../../molecules/graphs/ChatRateGraph";
+import ReplyCountByHourlyGraph from "../../molecules/graphs/ReplyCountByHourlyGraph";
+import KeywordChartGraph from "../../molecules/graphs/KeywordChartGraph";
+import ReplySpeedGraph from "../../molecules/graphs/ReplySpeedGraph";
+import ChatVolumeByHourlyGraph from "../../molecules/graphs/ChatVolumeByHourlyGraph";
 import { CgMaximize } from "react-icons/cg";
-import { setIsModalVisible } from "../../store/reducer/isModalVisibleSlice";
-import { FlexColumnCenterDiv } from "../atoms/FlexDiv";
+import { FlexColumnCenterDiv } from "../../atoms/FlexDiv";
+import { setIsModalVisible } from "../../../store/reducer/isModalVisibleSlice";
 
 const graphContentData = [
   {

src/components/organisms/dashboard/ModalGraph.tsx
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import Span from "../atoms/Span";
-import Icon from "../atoms/Icon";
-import ChatRatioWithArrowGraph from "../molecules/graphs/ChatRatioWithArrowGraph";
-import SpeakerSelect from "../molecules/SpeakerSelect";
+import Span from "../../atoms/Span";
+import Icon from "../../atoms/Icon";
+import ChatRatioWithArrowGraph from "../../molecules/graphs/ChatRatioWithArrowGraph";
+import SpeakerSelect from "../../molecules/dashboard/SpeakerSelect";
 import { useLocation } from "react-router";
 import { MdClose } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
-import { AnalyzedMessage } from "../../@types/index.d";
-import { getDates } from "../../module/common/getProperties";
-import { setIsModalVisible } from "../../store/reducer/isModalVisibleSlice";
-import { graphContentData } from "../pages/DetailPage";
+import { AnalyzedMessage } from "../../../@types/index.d";
+import { getDates } from "../../../module/common/getProperties";
+import { setIsModalVisible } from "../../../store/reducer/isModalVisibleSlice";
+import { graphContentData } from "../../pages/DetailPage";
 import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
-import { FlexCenterDiv } from "../atoms/FlexDiv";
-import Paragraph from "../atoms/Paragraph";
+import { FlexCenterDiv } from "../../atoms/FlexDiv";
+import Paragraph from "../../atoms/Paragraph";
 
 const ModalGraphBox = styled.div`
   padding: 20px 20px 30px 20px;

src/components/organisms/main/MainVisual.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import Paragraph from "../atoms/Paragraph";
-import Img from "../atoms/Img";
+import Paragraph from "../../atoms/Paragraph";
+import Img from "../../atoms/Img";
 import { useNavigate } from "react-router-dom";
-import ScrollIndicator from "../molecules/ScrollIndicator";
-import BlueButton from "../atoms/BlueButton";
-import { FlexCenterDiv } from "../atoms/FlexDiv";
+import { FlexCenterDiv } from "../../atoms/FlexDiv";
+import ScrollIndicator from "../../molecules/ScrollIndicator";
+import BlueButton from "../../atoms/BlueButton";
 import { useSelector } from "react-redux";
 
 const MainVisualContainer = styled(FlexCenterDiv)`
@@ -51,7 +51,9 @@ const AsideBox = styled.div`
     position: absolute;
     left: 50%;
     transform: translateX(-50%);
-    bottom: -50px;
+    @media (max-width: 768px) {
+      bottom: 0px;
+    }
   }
 `;
 

src/components/sections/attachment/AttachDescriptionSection.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
-import BlueButton from "../atoms/BlueButton";
-import Paragraph from "../atoms/Paragraph";
-import ThreeImages, { CardData } from "../organisms/ThreeImages";
-import scrollToEvent from "../../module/common/scrollToEvent";
-import { FlexCenterDiv, FlexColumnCenterDiv } from "../atoms/FlexDiv";
+import BlueButton from "../../atoms/BlueButton";
+import Paragraph from "../../atoms/Paragraph";
+import ThreeImages, { CardData } from "../../organisms/attachment/ThreeImages";
+import scrollToEvent from "../../../module/common/scrollToEvent";
+import { FlexCenterDiv, FlexColumnCenterDiv } from "../../atoms/FlexDiv";
 import { useLocation } from "react-router";
 
 const AttachDescriptionBox = styled(FlexColumnCenterDiv)`

src/components/sections/attachment/AttachmentSection.tsx
@@ -1,16 +1,16 @@
 import React, { useRef } from "react";
 import styled from "styled-components";
-import AttachedFileList from "../molecules/AttachedFileList";
-import BlueButton from "../atoms/BlueButton";
-import ScrollIndicator from "../molecules/ScrollIndicator";
-import FileDrop from "../organisms/FileDrop";
+import AttachedFileList from "../../molecules/attachment/AttachedFileList";
+import BlueButton from "../../atoms/BlueButton";
+import ScrollIndicator from "../../molecules/ScrollIndicator";
+import FileDrop from "../../organisms/attachment/FileDrop";
 import {
   AnalyzedMessage,
   Chatroom,
   FileObject,
   MessageInfo,
   OriginMessageData,
-} from "../../@types/index.d";
+} from "../../../@types/index.d";
 
 import {
   breakdownTxtFileAndroid,
@@ -18,18 +18,18 @@ import {
   breakdownTxtFileMacOS,
   breakdownTxtFileWindow,
   readAsDataURL,
-} from "../../module/core/breakdownTxtFile";
-import { getMessageData } from "../../module/core/getMessageData";
+} from "../../../module/core/breakdownTxtFile";
+import { getMessageData } from "../../../module/core/getMessageData";
 import { useDispatch, useSelector } from "react-redux";
-import { setAnalyzedMessages } from "../../store/reducer/analyzedMessagesSlice";
-import Span from "../atoms/Span";
+import { setAnalyzedMessages } from "../../../store/reducer/analyzedMessagesSlice";
+import Span from "../../atoms/Span";
 import { useNavigate } from "react-router";
-import scrollToEvent from "../../module/common/scrollToEvent";
-import OsList from "../organisms/OsList";
-import { pushNewlyAttachedFiles } from "../../store/reducer/attachedFileListSlice";
-import { setIsAnalyzedMessagesExist } from "../../store/reducer/isAnalyzedMessagesExistSlice";
-import Paragraph from "../atoms/Paragraph";
-import { FlexColumnCenterDiv } from "../atoms/FlexDiv";
+import scrollToEvent from "../../../module/common/scrollToEvent";
+import { pushNewlyAttachedFiles } from "../../../store/reducer/attachment/attachedFileListSlice";
+import { setIsAnalyzedMessagesExist } from "../../../store/reducer/isAnalyzedMessagesExistSlice";
+import Paragraph from "../../atoms/Paragraph";
+import { FlexColumnCenterDiv } from "../../atoms/FlexDiv";
+import OsList from "../../organisms/attachment/OsList";
 
 const AttachmentSectionBox = styled(FlexColumnCenterDiv)`
   margin: 80px auto 0 auto;